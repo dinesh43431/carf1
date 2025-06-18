c83f35813e74d82b2bf4d2f5a527f67085d0fee6
A.Dinesh Kumar <dinesh.arige@msitprogram.net>
2025-06-18 21:09:28 +0530

    Implement responsive navigation and layout (Challenge 5)

---
+++ challenge-5/blog-form/src/App.js
@@ -0,0 +1,26 @@
+// src/App.js
+import React, { useState } from 'react';
+import BlogPostForm from './components/BlogPostForm/BlogPostForm';
+import BlogList from './components/BlogList';
+import Layout from './components/Layout/Layout';
+
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  const handleBlogPostSubmit = async (data) => {
+    setPosts((prev) => [...prev, data]);
+  };
+
+  const handleDelete = (idx) => {
+    setPosts((prev) => prev.filter((_, i) => i !== idx));
+  };
+
+  return (
+    <Layout>
+      <BlogPostForm onSubmit={handleBlogPostSubmit} />
+      <BlogList posts={posts} onDelete={handleDelete} />
+    </Layout>
+  );
+}
+
+export default App;
